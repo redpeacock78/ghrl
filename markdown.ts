77b2a1a06a1a4b5d22b05fa0913df8fe5138638e
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-16 01:52:16 +0900

    chore: Change markdown.ts

--- markdown.ts
+++ markdown.ts
@@ -7,13 +7,18 @@ export async function index(): Promise<string> {
   const markup = Marked.parse(markdown);
   const result: string = `<link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/github-dark.min.css">
-    <body class=".dark-mode dark-mode">
+    <body class="d-flex dark-mode">
       <div class="page-wrapper">
         <div class="content-wrapper">
           <div class="container-fluid">
             <div class="content">
               <div class="card">
                   ${markup.content.replace(/<code>/g, '<code class="hljs">').replace('<code class="lang-bash">', '<code class="bash hljs">')}
+                  <footer style="text-align: center;">
+                    <p>
+                      © 2021, <a href="https://github.com/redpeacock78">redpeacock78</a>, <a href="https://github.com/redpeacock78/ghrl/blob/master/LICENSE">MIT License</a>, <a href="https://github.com/redpeacock78/ghrl">Repository</a>
+                    </p>
+                  </footer>
               </div>
             </div>
           </div>
