174afee0907867c4391233480bd41907ba490d7a
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-16 23:17:34 +0900

    feat: Change deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
-export { opine } from "https://deno.land/x/opine@1.9.1/mod.ts";
+import Aqua from "https://deno.land/x/aqua@v1.3.2/deploy.ts";
+export { Aqua };
 export { latest } from "./latest.ts";
 export { tagging } from "./tagging.ts";
 export { index } from "./markdown.ts";
