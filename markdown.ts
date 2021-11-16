89394dfd51a6d15231ff07aa98c1e6bac4c53a84
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-16 12:55:01 +0900

    chore: Change markdown.ts

--- markdown.ts
+++ markdown.ts
@@ -5,20 +5,47 @@ export async function index(): Promise<string> {
   const readme = await Deno.readFile("./README.md");
   const markdown = decoder.decode(readme);
   const markup = Marked.parse(markdown);
-  const result: string = `<link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/github-dark.min.css">
-    <body class="d-flex dark-mode">
+  const result: string = `
+  <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+    <link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/github-dark.min.css">
+    <meta property="og:title" content="Ghrl" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.ghrl.tk/" />
+    <meta property="og:site_name" content="Ghrl -A service to retrieve Github releases assets through Deno🦕-" />
+    <meta property="og:image" content="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🦕</text></svg>" />
+    <meta property="og:description" content="This service retrieves assets distributed by Github Releases through Deno🦕" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@kazuki_199778" />
+    <meta name="twitter:player" content="@kazuki_199778" />
+    <meta name="twitter:tilte" content="Ghrl -A service to retrieve Github releases assets through Deno🦕-" />
+    <meta name="twitter:image" content="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🦕</text></svg>" />
+    <meta name="twitter:description" content="This service retrieves assets distributed by Github Releases through Deno🦕" />
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🦕</text></svg>">
+    <title>Ghrl -A service to retrieve Github releases assets through Deno🦕-</title>
+  </head>
+    <body class="dark-mode">
       <div class="page-wrapper">
         <div class="content-wrapper">
           <div class="container-fluid">
-            <div class="content">
+            <div class="mw-full">
               <div class="card">
-                  ${markup.content.replace(/<code>/g, '<code class="hljs">').replace('<code class="lang-bash">', '<code class="bash hljs">')}
+                <div class="content">
+                  ${markup.content
+                    .replace(/<code>/g, '<code class="hljs">')
+                    .replace(/<pre>/g, '<div class="code-container">\n<pre>')
+                    .replace(/<\/pre>/g, "</pre>\n</div>")
+                    .replace('<code class="lang-bash">', '<code class="bash hljs">')
+                    .replace(/<p>/g, '<p class="text-justify font-size-14">')
+                    .replace(/<h(2|3) id="-.*">/g, '<h2 class="content-title">')}
                   <footer style="text-align: center;">
                     <p>
-                      © 2021, <a href="https://github.com/redpeacock78">redpeacock78</a>, <a href="https://github.com/redpeacock78/ghrl/blob/master/LICENSE">MIT License</a>, <a href="https://github.com/redpeacock78/ghrl">Repository</a>
+                      © 2021, <a href="https://github.com/redpeacock78">redpeacock78</a>, 
+                      <a href="https://github.com/redpeacock78/ghrl/blob/master/LICENSE">MIT License</a>, 
+                      <a href="https://github.com/redpeacock78/ghrl">Repository</a>
                     </p>
                   </footer>
+                </div>
               </div>
             </div>
           </div>
@@ -27,6 +54,7 @@ export async function index(): Promise<string> {
     </body>
     <script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/js/halfmoon.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
-    <script>hljs.highlightAll();</script>`;
+    <script>hljs.highlightAll();</script>
+  `;
   return result;
 }
