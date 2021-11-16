19b434ea71bd23f067a6b0fa4e4c890b9875d86b
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-16 23:18:03 +0900

    feat: Change server.ts

--- server.ts
+++ server.ts
@@ -1,41 +1,53 @@
-import { opine, latest, tagging, index } from "./deps.ts";
+import { Aqua, latest, tagging, index } from "./deps.ts";
 
-const app = opine();
+const app = new Aqua();
 
 // Root index
-app.get("/", async (req, res): Promise<void> => {
+app.get("/", async (): Promise<string> => {
   const root: string = await index();
-  res.send(root);
+  return root;
 });
 
 // Download source code
-app.get("/:author/:repo", async (req, res): Promise<void> => {
-  const author: string = req.params.author;
-  const repo: string = req.params.repo.match(/@.+$/) ? req.params.repo.substring(0, req.params.repo.indexOf("@")) : req.params.repo;
-  const tag: string | null = req.params.repo.match(/@.+$/) ? req.params.repo.substring(req.params.repo.indexOf("@")).replace("@", "") : null;
+app.get("/:author/:repo", async (req): Promise<any> => {
+  const author: string = req.parameters.author;
+  const repo: string = req.parameters.repo.match(/@.+$/) ? req.parameters.repo.substring(0, req.parameters.repo.indexOf("@")) : req.parameters.repo;
+  const tag: string | null = req.parameters.repo.match(/@.+$/) ? req.parameters.repo.substring(req.parameters.repo.indexOf("@")).replace("@", "") : null;
   const source: string = req.query.source;
   if (author && repo && !source) {
     if (!tag) {
       // Latest
       // /:author/:repo
       const params_arr = { author: author, repo: repo };
-      await latest(params_arr)
-        .then((i: string): void => {
-          res.redirect(i);
+      return await latest(params_arr)
+        .then((i: string) => {
+          return {
+            statusCode: 302,
+            redirect: i,
+          };
         })
-        .catch((e: Error): void => {
-          res.setStatus(404).send({ Error: e.message });
+        .catch((e: Error) => {
+          return {
+            statusCode: 404,
+            content: { Error: e.message },
+          };
         });
     } else {
       // Tagging
       // /:author/:repo@tag
       const params_arr = { author: author, repo: repo, tag: tag };
-      await tagging(params_arr)
-        .then((i: string): void => {
-          res.redirect(i);
+      return await tagging(params_arr)
+        .then((i: string) => {
+          return {
+            statusCode: 302,
+            redirect: i,
+          };
         })
-        .catch((e: Error): void => {
-          res.setStatus(404).send({ Error: e.message });
+        .catch((e: Error) => {
+          return {
+            statusCode: 404,
+            content: { Error: e.message },
+          };
         });
     }
   } else if (author && repo && source) {
@@ -43,59 +55,81 @@ app.get("/:author/:repo", async (req, res): Promise<void> => {
       // Latest
       // /:author/:repo?source=zip
       const params_arr = { author: author, repo: repo, source: source };
-      await latest(params_arr)
-        .then((i: string): void => {
-          res.redirect(i);
+      return await latest(params_arr)
+        .then((i: string) => {
+          return {
+            statusCode: 302,
+            redirect: i,
+          };
         })
-        .catch((e: Error): void => {
-          res.setStatus(404).send({ Error: e.message });
+        .catch((e: Error) => {
+          return {
+            statusCode: 404,
+            content: { Error: e.message },
+          };
         });
     } else {
       // Tagging
       // /:author/:repo@tag?source=zip
       const params_arr = { author: author, repo: repo, tag: tag, source: source };
-      await tagging(params_arr)
-        .then((i: string): void => {
-          res.redirect(i);
+      return await tagging(params_arr)
+        .then((i: string) => {
+          return {
+            statusCode: 302,
+            redirect: i,
+          };
         })
-        .catch((e: Error): void => {
-          res.setStatus(404).send({ Error: e.message });
+        .catch((e: Error) => {
+          return {
+            statusCode: 404,
+            content: { Error: e.message },
+          };
         });
     }
   }
 });
 
 // Download assets
-app.get("/:author/:repo/:assets_name", async (req, res): Promise<void> => {
-  const author: string = req.params.author;
-  const repo: string = req.params.repo.match(/@.+$/) ? req.params.repo.substring(0, req.params.repo.indexOf("@")) : req.params.repo;
-  const tag: string | null = req.params.repo.match(/@.+$/) ? req.params.repo.substring(req.params.repo.indexOf("@")).replace("@", "") : null;
-  const assets_name: string = req.params.assets_name;
+app.get("/:author/:repo/:assets_name", async (req): Promise<any> => {
+  const author: string = req.parameters.author;
+  const repo: string = req.parameters.repo.match(/@.+$/) ? req.parameters.repo.substring(0, req.parameters.repo.indexOf("@")) : req.parameters.repo;
+  const tag: string | null = req.parameters.repo.match(/@.+$/) ? req.parameters.repo.substring(req.parameters.repo.indexOf("@")).replace("@", "") : null;
+  const assets_name: string = req.parameters.assets_name;
   if (author && repo && assets_name) {
     if (!tag) {
       // Latest
       // /:author/:repo/:assets_name
       const params_arr = { author: author, repo: repo, assets_name: assets_name };
-      await latest(params_arr)
-        .then((i: string): void => {
-          res.redirect(i);
+      return await latest(params_arr)
+        .then((i: string) => {
+          return {
+            statusCode: 302,
+            redirect: i,
+          };
         })
-        .catch((e: Error): void => {
-          res.setStatus(404).send({ Error: e.message });
+        .catch((e: Error) => {
+          return {
+            statusCode: 404,
+            content: { Error: e.message },
+          };
         });
     } else {
       // Tagging
       // /:author/:repo@tag/:assets_name
       const params_arr = { author: author, repo: repo, tag: tag, assets_name: assets_name };
-      await tagging(params_arr)
-        .then((i: string): void => {
-          res.redirect(i);
+      return await tagging(params_arr)
+        .then((i: string) => {
+          return {
+            statusCode: 302,
+            redirect: i,
+          };
         })
-        .catch((e: Error): void => {
-          res.setStatus(404).send({ Error: e.message });
+        .catch((e: Error) => {
+          return {
+            statusCode: 404,
+            content: { Error: e.message },
+          };
         });
     }
   }
 });
-
-app.listen(8080);
