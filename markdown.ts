9b3e1611702d40a15c81e65179f64dba80ce4bb4
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-18 01:48:12 +0900

    chore: Change markdown.ts

--- markdown.ts
+++ markdown.ts
@@ -11,7 +11,7 @@ export async function index(): Promise<string> {
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/base16/solarized-dark.min.css">
-    <meta property="og:title" content="Ghrl" />
+    <meta property="og:title" content="Ghrl -A service to retrieve 'Github Releases' assets through Deno Deploy🦕-" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://ghrl.tk/" />
     <meta property="og:site_name" content="Ghrl -A service to retrieve 'Github Releases' assets through Deno Deploy🦕" />
@@ -20,7 +20,7 @@ export async function index(): Promise<string> {
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@kazuki_199778" />
     <meta name="twitter:player" content="@kazuki_199778" />
-    <meta name="twitter:tilte" content="Ghrl -A service to retrieve 'Github Releases' assets through Deno Deploy🦕" />
+    <meta name="twitter:tilte" content="Ghrl -A service to retrieve 'Github Releases' assets through Deno Deploy🦕-" />
     <meta name="twitter:image" content="https://i.imgur.com/IeeR9d7.png" />
     <meta name="twitter:description" content="A service to retrieve 'Github Releases' assets through Deno Deploy🦕" />
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🦕</text></svg>">
