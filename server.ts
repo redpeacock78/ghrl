f312145eacd7fd4d1a67d5a2746cfafb91aff366
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-18 14:44:13 +0900

    chore: Change server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
-import { Aqua, latest, tagging, index } from "./deps.ts";
+// deno-lint-ignore-file no-explicit-any camelcase
+import { Aqua, index, latest, tagging } from "./deps.ts";
 
 const app = new Aqua();
 
@@ -15,8 +16,15 @@ app.get("/", async (): Promise<any> => {
 // Download source code
 app.get("/:author/:repo", async (req): Promise<any> => {
   const author: string = req.parameters.author;
-  const repo: string = req.parameters.repo.match(/@.+$/) ? req.parameters.repo.substring(0, req.parameters.repo.indexOf("@")) : req.parameters.repo;
-  const tag: string | null = req.parameters.repo.match(/@.+$/) ? req.parameters.repo.substring(req.parameters.repo.indexOf("@")).replace("@", "") : null;
+  const repo: string = req.parameters.repo.match(/@.+$/)
+    ? req.parameters.repo.substring(0, req.parameters.repo.indexOf("@"))
+    : req.parameters.repo;
+  const tag: string | null = req.parameters.repo.match(/@.+$/)
+    ? req.parameters.repo.substring(req.parameters.repo.indexOf("@")).replace(
+      "@",
+      "",
+    )
+    : null;
   const source: string = req.query.source;
   if (!tag) {
     // Latest
@@ -37,7 +45,12 @@ app.get("/:author/:repo", async (req): Promise<any> => {
   } else {
     // Tagging
     // /:author/:repo@tag[?source=zip]
-    return await tagging({ author: author, repo: repo, source: source, tag: tag })
+    return await tagging({
+      author: author,
+      repo: repo,
+      source: source,
+      tag: tag,
+    })
       .then((i: string) => {
         return {
           statusCode: 302,
@@ -56,13 +69,24 @@ app.get("/:author/:repo", async (req): Promise<any> => {
 // Download assets
 app.get("/:author/:repo/:assets_name", async (req): Promise<any> => {
   const author: string = req.parameters.author;
-  const repo: string = req.parameters.repo.match(/@.+$/) ? req.parameters.repo.substring(0, req.parameters.repo.indexOf("@")) : req.parameters.repo;
-  const tag: string | null = req.parameters.repo.match(/@.+$/) ? req.parameters.repo.substring(req.parameters.repo.indexOf("@")).replace("@", "") : null;
+  const repo: string = req.parameters.repo.match(/@.+$/)
+    ? req.parameters.repo.substring(0, req.parameters.repo.indexOf("@"))
+    : req.parameters.repo;
+  const tag: string | null = req.parameters.repo.match(/@.+$/)
+    ? req.parameters.repo.substring(req.parameters.repo.indexOf("@")).replace(
+      "@",
+      "",
+    )
+    : null;
   const assets_name: string = req.parameters.assets_name;
   if (!tag) {
     // Latest
     // /:author/:repo/:assets_name
-    return await latest({ author: author, repo: repo, assets_name: assets_name })
+    return await latest({
+      author: author,
+      repo: repo,
+      assets_name: assets_name,
+    })
       .then((i: string) => {
         return {
           statusCode: 302,
@@ -78,7 +102,12 @@ app.get("/:author/:repo/:assets_name", async (req): Promise<any> => {
   } else {
     // Tagging
     // /:author/:repo@tag/:assets_name
-    return await tagging({ author: author, repo: repo, tag: tag, assets_name: assets_name })
+    return await tagging({
+      author: author,
+      repo: repo,
+      tag: tag,
+      assets_name: assets_name,
+    })
       .then((i: string) => {
         return {
           statusCode: 302,
