629108f31282ee430ce7d2912baa783488ba2c72
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-19 01:09:25 +0900

    chore: Change deps.ts

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-import Aqua from 'https://deno.land/x/aqua@v1.3.2/deploy.ts';
-import ky from 'https://cdn.skypack.dev/ky@0.28.7?dts';
+import Aqua from "https://deno.land/x/aqua@v1.3.2/deploy.ts";
+import ky from "https://cdn.skypack.dev/ky@0.28.7?dts";
 export { Aqua, ky };
-export { latest } from './latest.ts';
-export { tagging } from './tagging.ts';
-export { index } from './markdown.ts';
+export { latest } from "./latest.ts";
+export { tagging } from "./tagging.ts";
+export { index } from "./markdown.ts";
