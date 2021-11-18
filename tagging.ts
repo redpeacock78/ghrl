3d77009527d05387868e890557d434c8629bbafe
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-18 14:43:25 +0900

    chore: Change tagging.ts

--- tagging.ts
+++ tagging.ts
@@ -1,9 +1,18 @@
-import ky from "https://cdn.skypack.dev/ky?dts";
+// deno-lint-ignore-file camelcase no-inferrable-types
+import { ky } from "./deps.ts";
 
 // https://example.com/:author/:repo@tag
 // https://example.com/:author/:repo@tag?source=zip
 // https://example.com/:author/:repo@tag/:assets_name
-export async function tagging(params: { author: string; repo: string; tag: string; assets_name?: string; source?: string }) {
+export async function tagging(
+  params: {
+    author: string;
+    repo: string;
+    tag: string;
+    assets_name?: string;
+    source?: string;
+  },
+) {
   const author: string = params.author;
   const repo_name: string = params.repo;
   const tag: string = params.tag;
@@ -12,26 +21,41 @@ export async function tagging(params: { author: string; repo: string; tag: strin
   if (source && source !== "zip") {
     throw new Error("Only zip can be specified in the source parameter.");
   }
-  const tagging_api: string = `https://api.github.com/repos/${author}/${repo_name}/releases`;
+  const tagging_api: string =
+    `https://api.github.com/repos/${author}/${repo_name}/releases`;
   return await ky
     .get(tagging_api)
     .then(async (i: Response): Promise<string> => {
-      const resp = (await i.json()) as unknown as [{ tag_name: string; assets: unknown[]; tarball_url: string; zipball_url: string }];
-      const tag_arr: string[] = resp.map((i: { tag_name: string; assets: unknown[] }): string => i.tag_name);
+      const resp = (await i.json()) as unknown as [
+        {
+          tag_name: string;
+          assets: unknown[];
+          tarball_url: string;
+          zipball_url: string;
+        },
+      ];
+      const tag_arr: string[] = resp.map((
+        i: { tag_name: string; assets: unknown[] },
+      ): string => i.tag_name);
       if (tag_arr.indexOf(tag) !== -1) {
-        const releases_urls: string[] = (resp[tag_arr.indexOf(tag)].assets as [{ browser_download_url: string }]).map((i): string => {
+        const releases_urls: string[] = (resp[tag_arr.indexOf(tag)].assets as [
+          { browser_download_url: string },
+        ]).map((i): string => {
           return i.browser_download_url;
         });
         if (assets_name) {
-          const assets_arr: string[] = releases_urls.map((i: string): string => {
-            return i.split("/")[8];
-          });
+          const assets_arr: string[] = releases_urls.map(
+            (i: string): string => {
+              return i.split("/")[8];
+            },
+          );
           const assets_name_judge: string[] = assets_arr
             .map((i: string, n: number): string => {
               return i === `${assets_name}` ? n.toString() : "";
             })
             .filter(Boolean) as string[];
-          const assets_url: string = releases_urls[parseInt(assets_name_judge.join(""))];
+          const assets_url: string =
+            releases_urls[parseInt(assets_name_judge.join(""))];
           if (typeof assets_url === "string") {
             return assets_url;
           } else {
